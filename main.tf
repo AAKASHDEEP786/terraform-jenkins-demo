775dfe2e1a1e84b3e8cadcf089d763f66af8be61
Aakashdeep <akashdeep960@gmail.com>
2026-06-11 13:31:06 +0530

    Added code

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "web" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["106.206.197.40/32"]
   }
 
   egress {
